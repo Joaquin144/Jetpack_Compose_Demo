a4b965c292978dae28bfde64df1c970563972f4c
Joaquin144 <vvpp898.comco@gmail.com>
2022-01-20 21:51:51 +0530

    Modifier Done

--- app/src/main/java/com/vibhu/nitjsr/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/vibhu/nitjsr/jetpackcomposedemo/MainActivity.kt
@@ -3,36 +3,52 @@ package com.vibhu.nitjsr.jetpackcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vibhu.nitjsr.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
-
+/*
+                Notes :------
+1.> Alignment : The horizontal alignment of the layout's children
+2.> Arrangement : The vertical arrangement of the layout's children
+3.> Main axis(arrangement) and Cross Axis(alignment)
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
+            Column(
+                modifier = Modifier
+                    .background(Color.Blue)
+                    .width(250.dp)
+                    .height(500.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("Sinaloa")
+                Text(text = "Golden Triangle")
             }
+
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeDemoTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        Greeting("Vibhu")
     }
 }
